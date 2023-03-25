100f87fb1ee8e047bf2fe045ad107d092a1e812d
Muk74dir <muktadir.sarker99@gmail.com>
2023-03-25 21:24:13 +0600

    TIME LIMIT EXCEEDED

--- Introductory Problems/Increasing_Array.cpp
+++ Introductory Problems/Increasing_Array.cpp
@@ -1,24 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int fact(int x){
+    if(x==1)return 1;
+    return x*fact(x-1);
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    set<char>dis;
 
-    int n;cin>>n;
-    int arr[n];
-    long long int cnt=0;
+    string s;cin>>s;
 
-    for(int i=0; i<n; i++)
-        cin>>arr[i];
+    int rem[dis.size()];
 
-    for(int j=1; j<n; j++){
-        if(arr[j-1] > arr[j]){
-            cnt += (arr[j-1] - arr[j]);
-            arr[j] += (arr[j-1] - arr[j]);
-        }
+    for(int i=0; i<s.size(); i++){
+        dis.insert(s[i]);
     }
-    cout<<cnt;
+
+    for(int i=0; i<dis.size(); i++){
+        cout<<count(s.begin(), s.end(), s[i]);
+    }
+
+
+
+    cout<<fact(dis.size())<<endl;
+
     return 0;
 }
